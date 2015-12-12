f62442305241f89cec408bb99af01d6e929f50b7
Sergey Lonshakov <sergeylonshakov@gmail.com>
2015-12-13 00:36:56 +0400

    add marketAgent

--- dao-core.sol
+++ dao-core.sol
@@ -16,8 +16,6 @@ contract token {
     event AddressApproval(address indexed _address, address indexed _proxy, bool _result);
     event AddressApprovalOnce(address indexed _address, address indexed _proxy, uint256 _value);
 
-
-
     /*Initial */
     function token() {
         creator = msg.sender;
@@ -99,38 +97,39 @@ contract token {
         return true;
     }
 
-    function unapprove(address _address) returns (bool success) {
+    function unapprove(address _address) returns (bool result) {
         approveOf[msg.sender][_address] = false;
         return true;        
     }
 
-    function approveOnce(address _address, uint256 _maxValue) returns (bool success) {
+    function approveOnce(address _address, uint _maxValue) returns (bool result) {
         approveOnceOf[msg.sender][_address] = true;
         approveOnceValueOf[msg.sender][_address] = _maxValue;
         AddressApprovalOnce(_address, msg.sender, _maxValue);
         return true;       
     }
 
-    function unapproveOnce(address _address) returns (bool success) {
+    function unapproveOnce(address _address) returns (bool result) {
         approveOnceOf[msg.sender][_address] = false;
         approveOnceValueOf[msg.sender][_address] = 0;
         return true;       
     }
 
-    function isApprovedOnceFor(address _target, address _proxy) returns (uint256 maxValue) {
+    function isApprovedOnceFor(address _target, address _proxy) returns (uint maxValue) {
         maxValue = approveOnceValueOf[_target][_proxy];
         return maxValue;
     }
 
-    function isApprovedFor(address _target, address _proxy) constant returns (bool success) {
-        success = approveOnceOf[_target][_proxy];
-        return success;
+    function isApprovedFor(address _target, address _proxy) constant returns (bool result) {
+        result = approveOnceOf[_target][_proxy];
+        return result;
     }
 }
 
+
 contract agent {
     address creator;
-    
+
     modifier creatorCheck { if (msg.sender == creator) _ }
 
     /* Struct */
@@ -142,13 +141,12 @@ contract agent {
         string helper;
     }
 
-
     /* Functions */
     function agent() {
         creator = msg.sender;
     }
 
-    function setAgentContract(address _agentContractAddr, address _abi, address _helper) creatorCheck returns(uint agentContractID) {
+    function setAgentContract(address _agentContractAddr, string _abi, string _helper) creatorCheck returns(uint agentContractID) {
         agentContractID = agentContracts.length++;
         AgentContract a = agentContracts[agentContractID];
         a.agentContractAddr = _agentContractAddr;
@@ -158,203 +156,83 @@ contract agent {
     }    
 }
 
-contract DAO {
-    address public daoFounder;
-    address public daoFounderContr;
-    address public daoMarketContr;
-    address public daoGovermentContr;
-    bool public initialization;
-    uint public creditAmount;
-    uint public sharesAmount;
-    uint public creditTurn;
-    uint public numAgents;
+contract marketAgent is agent {
+    market agentMarket;
+    token credit;
+    
+    modifier creatorCheck { if (msg.sender == creator) _ }
 
-    token public daoShares;
-    token public daoCredits;
+    function marketAgent(address _marketAddr, string _marketAbi, string _marketHelper) {
+        creator = msg.sender;
+        agentMarket = market(_marketAddr);
+        credit.approve(agentMarket);
 
-    /* Assets data */
-    Asset[] public assetsList;
-    mapping (address => uint) public assetDataOf;
-    mapping (address => bool) public assetExistOf;
-    struct Asset {
-        address assetAddr;
-    }
-
-    /* Agents data */
-    Agent[] public agentsList;
-    mapping (address => uint) public agentDataOf; 
-    mapping (address => bool) public agentActiveOf;
-    struct Agent {
-        address agentContrAddr;
-        uint joinData;
-    }
-
-    function DAO(token _shares, token _credits, market _daoMarketContr, goverment _daoGovermentContr) {
-        daoFounder = msg.sender;  
-        daoShares = token(_shares);
-        daoCredits = token(_credits);
-        daoMarketContr = _daoMarketContr;
-        daoGovermentContr = _daoGovermentContr;
+        uint agentContractID = agentContracts.length++;
+        AgentContract a = agentContracts[agentContractID];
+        a.agentContractAddr = _marketAddr;
+        a.abi = _marketAbi;
+        a.helper = _marketHelper;
     }
     
-    function initializationDaoBalances(uint _founderSharesAmount, uint _founderCreditsAmount) returns (bool result) {
-        if(!initialization) {
-        daoShares.emission(msg.sender, _founderSharesAmount);
-        sharesAmount = _founderSharesAmount;
-        daoCredits.emission(msg.sender, _founderCreditsAmount);
-        creditAmount = _founderCreditsAmount;
-        return true;
-        }
-    }
-
-    function daoEfficiency() returns (uint daoEfficiency) {
-        daoEfficiency = creditTurn*100/creditAmount;
-    }
-
-    function daoShareCost() returns (uint shareCost) {
-        shareCost = creditTurn/sharesAmount;
-    }
-
-    function daoCreditPower() returns (uint creditPower) {
-        creditPower = sharesAmount*100/creditTurn;
-    }
-
-    function daoShareSale(uint _shareAmount) returns (uint creditReward) {
-        if(daoShares.tokenBalanceOf(msg.sender)>=_shareAmount) {
-            creditReward = daoShareCost()*_shareAmount;
-            daoCredits.emission(msg.sender, creditReward);
-            daoShares.burn(msg.sender, _shareAmount);
-        }
-    }
-
-    function daoShareBuy(uint _creditAmount) returns (uint shares) {
-        if(daoCredits.tokenBalanceOf(msg.sender)>=_creditAmount) {
-            shares = daoCreditPower()*_creditAmount;
-            daoShares.emission(msg.sender, shares);
-            daoCredits.burn(msg.sender, _creditAmount);
-        }
-    }
-
-    function daoSharesEmission(address _agentContrAddr, uint _sharesAmount) {
-        daoShares.emission(_agentContrAddr, _sharesAmount);
-    }
-
-    function daoCreditEmission(uint _creditsAMount) {
-        daoCredits.emission(msg.sender, _creditsAMount);
-    }
-
-
-    function setAgent(address _agentContAddr) returns(uint agentID) {
-        if(daoShares.tokenBalanceOf(msg.sender)>0) {
-            agentID = agentsList.length++;
-            Agent a = agentsList[agentID];
-            a.agentContrAddr = _agentContAddr;
-            a.joinData = now;
-            uint newAgentSharesAmount;
-            newAgentSharesAmount = sharesAmount/numAgents;
-            daoShares.emission(_agentContAddr, newAgentSharesAmount);
-            numAgents = agentID;   
-            agentDataOf[_agentContAddr] = agentID;
-            agentActiveOf[_agentContAddr] = true;
-            return agentID;
-        }
-    }
-
-    function setAssets(address _assetAddr) returns(uint assetID) {
-        if (msg.sender == daoMarketContr) {
-            assetID = assetsList.length++;
-            Asset a = assetsList[assetID];
-            a.assetAddr = _assetAddr;
-            assetDataOf[_assetAddr] = assetID;
-            assetExistOf[_assetAddr] = true;
-            return assetID;
-        }
+    function approveSupply(token _asset, uint _maxValue) creatorCheck returns(bool result) {
+        token asset = token(_asset);
+        result = asset.approveOnce(agentMarket, _maxValue);
+        return result;
     }
 }
 
-
 contract market {
-    address daoAddr;
-    DAO public dao;
+    address creator;
+    token credit;
 
     struct Order {
         uint orderID;
         address owner;
-        uint amount;
-        uint price;
+        uint total;
+        uint unitPrice;
+        uint min;
+        uint step;
+        bool active;
     }
 
-
-    struct OrderList {
+    struct SaleAssetList {
         address assetAddr;
-        Order[] orders;
-    }
-    
-    OrderList[] public sellList;
-    Order[] public sellOrders;
-    mapping (address => bool) sellExistOf;
-    mapping (address => uint) sellDataOf;
-    
-    OrderList[] public buyList;
-    Order[] public buyOrders;
-    mapping (address => bool) buyExistOf;
-    mapping (address => uint) buyDataOf;
-
-
-    function getSellList(address _assetAddr) returns(uint assetID) {
-        return 0;
+        Order[] sellOrderList;
     }
 
-    function getBuyList(address _assetAddr) returns(uint assetID) {
-        return 0;
+    struct BuyAssetList {
+        address assetAddr;
+        Order[] buyOrderList;
     }
 
-    function addSell(address _assetAddr, uint _amount, uint _price) returns(uint sellID) {
-        if (dao.agentActiveOf(msg.sender) && dao.assetExistOf(_assetAddr)) {
-            if(sellExistOf[_assetAddr]) {
-                uint assetID;
-                assetID = sellDataOf[_assetAddr];
-                OrderList[] assetOrders = sellList[assetID];
-                sellID = sellOrders.sellOrders.length++;
-                sellOrders.orders[sellID] = Order({orderID: sellID, owner: msg.sender, amount: _amount, price: _price});
-                return assetID;
-            }
-            
+    SaleAssetList[] public sellAssetList;
+    Order[] public sellOrderList;
+    mapping (address => bool) public sellExistOf;
+    mapping (address => uint) public sellDataOf;
+
+    BuyAssetList[] public buyAssetList;
+    Order[] public buyOrderList;
+    mapping (address => bool) public buyExistOf;
+    mapping (address => uint) public buyDataOf;
+
+    function market(address _credit) {
+        creator = msg.sender;
+        credit = token(_credit);
+    }
+
+    function getSell(address _assetAddr, uint _orderID) returns(address owner, uint total, uint unitPrice, uint min, uint step, bool active) {
+        if (sellExistOf[_assetAddr]) {
+            uint assetID = sellDataOf[_assetAddr];
+            Order order = sellAssetList[assetID].sellOrderList[_orderID];
+            return (order.owner, order.total, order.unitPrice, order.min, order.step, order.active);
         }
     }
 
-    function addBuy(address _assetAddr, uint _amount, uint _price) returns(uint buyID) {
-        if (dao.agentActiveOf(msg.sender) && dao.assetExistOf(_assetAddr)) {
-            if(buyExistOf[_assetAddr]) {
-                uint assetID;
-                assetID = buyDataOf[_assetAddr];
-                OrderList[] buyOrders = buyList[assetID];
-                buyID = buyOrders.orders.length++;
-                buyOrders.orders[buyID] = Order({orderID: buyID, owner: msg.sender, amount: _amount, price: _price});
-                return assetID;
-            }
-            
+    function getBuy(address _assetAddr, uint _orderID) returns(address owner, uint total, uint unitPrice, uint min, uint step, bool active) {
+        if (buyExistOf[_assetAddr]) {
+            uint assetID = buyDataOf[_assetAddr];
+            Order order = buyAssetList[assetID].buyOrderList[_orderID];
+            return (order.owner, order.total, order.unitPrice, order.min, order.step, order.active);
         }
     }
-
-    
-    function BuyDeal(address _assetAddr, uint _buyID) {
-        uint profit = msg.value*dao.daoEfficiency()/100;
-        dao.daoCredits.emission(daoAddr, profit);
-        return true;
-    }
-
-    function SellDeal(address _assetAddr, uint sellID) returns(bool result) {
-        return true;
-    }
-
-
-}
-
-contract goverment {
-    address daoAddr;
-    DAO public dao;
-
-
-
 }
