8b2ee0cfe96a5789420b5cb48f5548d3cb0d74a0
ensrationis <sergeylonshakov@gmail.com>
2015-12-23 21:09:01 +0400

    add data structure

--- thesaurus.sol
+++ thesaurus.sol
@@ -1,6 +1,54 @@
 contract thesaurus {
+	string name;
+	string desc;
+	address owner;
+
+	modifier ownerCheck { if (msg.sender == owner) _ }
+
+	struct Prop {
+		string itemprop;
+		string itemtype;
+		string desc;
+	}
+
+	Prop[] public propList;
+
+	struct Enum {
+		uint propListID;
+	}
 	
-	struct metadata {
+	struct Metadata {
+		string itemscope;
+		Enum  enumPropList;
+	}
+
+	Metadata[] schemaorgList;
+	/* Help you find itemscope proplist*/
+	mapping (bytes32 => uint) public itemscopeOf;
+
+	function thesaurus(string _name, string _desc) {
+		name = _name;
+		desc = _desc;
+		owner = msg.sender;
+	}
+
+	function setProp(string _itemprop, string _itemtype, string _desc) ownerCheck returns(uint propID) {
+		propID = propList.length++;
+        Prop p = propList[propID];
+        p.itemprop = _itemprop;
+        p.itemtype = _itemtype;
+        p.desc = _desc;
+        return propID;
+	}
+
+	function getProp(uint propID) returns(string itemprop, string itemtype, string desc){
 		
 	}
+	
+	function getTest(string _itemscope) returns(uint ID){
+		ID = itemscopeOf[sha3(_itemscope)];
+		return ID;
+	}
+
+
 }
