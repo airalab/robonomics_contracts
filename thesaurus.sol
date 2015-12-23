c452628d436c1b6cc1d4f13c0bc2d47e3b2bb1dd
ensrationis <sergeylonshakov@gmail.com>
2015-12-23 23:44:03 +0400

    add set and get functions

--- thesaurus.sol
+++ thesaurus.sol
@@ -1,7 +1,7 @@
 contract thesaurus {
-	string name;
-	string desc;
-	address owner;
+	string public name;
+	string public desc;
+	address public owner;
 
 	modifier ownerCheck { if (msg.sender == owner) _ }
 
@@ -9,9 +9,12 @@ contract thesaurus {
 		string itemprop;
 		string itemtype;
 		string desc;
+		string dataType;
 	}
 
 	Prop[] public propList;
+	mapping (bytes32 => uint) public itempropOf;
+	mapping (bytes32 => bool) public itempropExistOf;
 
 	struct Enum {
 		uint propListID;
@@ -19,12 +22,14 @@ contract thesaurus {
 	
 	struct Metadata {
 		string itemscope;
-		Enum  enumPropList;
+		Enum[]  enumPropList;
+		uint numProp;
 	}
 
 	Metadata[] schemaorgList;
 	/* Help you find itemscope proplist*/
 	mapping (bytes32 => uint) public itemscopeOf;
+	mapping (bytes32 => bool) public itemscopeExistOf;
 
 	function thesaurus(string _name, string _desc) {
 		name = _name;
@@ -32,22 +37,60 @@ contract thesaurus {
 		owner = msg.sender;
 	}
 
-	function setProp(string _itemprop, string _itemtype, string _desc) ownerCheck returns(uint propID) {
+	function setProp(string _itemprop, string _itemtype, string _desc, string _dataType) ownerCheck returns(uint propID) {
 		propID = propList.length++;
         Prop p = propList[propID];
         p.itemprop = _itemprop;
         p.itemtype = _itemtype;
         p.desc = _desc;
+        p.dataType = _dataType;
+        itempropExistOf[sha3(p.itemprop)] = true;
+        itempropOf[sha3(p.itemprop)] = propID;
         return propID;
 	}
 
-	function getProp(uint propID) returns(string itemprop, string itemtype, string desc){
-		
+	function getPropID(string _itemprop) returns(bool result, uint propID){
+		if(!itempropExistOf[sha3(_itemprop)]) break;
+		result = true; 
+		propID = itempropOf[sha3(_itemprop)];
+		return(result, propID);
+	}
+
+	function getProp(uint _propID) returns(string itemprop, string itemtype, string desc, string dataType){
+		Prop p = propList[_propID];
+		return(p.itemprop, p.itemtype, p.desc, p.dataType);
 	}
 	
-	function getTest(string _itemscope) returns(uint ID){
-		ID = itemscopeOf[sha3(_itemscope)];
-		return ID;
+	function setMetadata(string _itemscope, uint _propID) ownerCheck returns(uint enumPropListID) {
+		uint metadataID;
+		if(!itemscopeExistOf[sha3(_itemscope)]) {
+			metadataID = schemaorgList.length++;    	
+        	itemscopeExistOf[sha3(m.itemscope)] = true;
+        	itemscopeOf[sha3(m.itemscope)] = metadataID;
+    	} else {
+    		metadataID = itemscopeOf[sha3(_itemscope)];
+    	}
+
+    	Metadata m = schemaorgList[metadataID];
+        m.itemscope = _itemscope;
+
+        enumPropListID = m.enumPropList.length++;
+       	Enum e = m.enumPropList[enumPropListID];
+        e.propListID = _propID;
+        m.numProp +=1;
+    	return enumPropListID;
+	}
+
+	function getMetadataID(string _itemscope) returns(bool result, uint metadataID){
+		if(!itemscopeExistOf[sha3(_itemscope)]) break;
+		result = true; 
+		metadataID = itemscopeOf[sha3(_itemscope)];
+		return(result, metadataID);
+	}
+
+	function getMetadataNumProp(uint _itemscopeID) returns(string itemscope, uint numProp){
+		Metadata m = schemaorgList[_itemscopeID];
+		return(m.itemscope, m.numProp);
 	}
 
 
