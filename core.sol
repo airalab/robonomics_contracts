c0489b8af150caccff26ff19481ccf6c75229087
EnsRationis <sergeylonshakov@gmail.com>
2016-01-13 16:59:00 +0400

    Prepare tag 0.3.0
    add DaoNode struct

--- core.sol
+++ core.sol
@@ -1,13 +1,20 @@
 contract core {
-	string daoName;
-	string desc;
-	string owner;
+	string public daoName;
+	string public desc;
+	address public admin;
 
-	modifier ownerCheck { if (msg.sender == owner) _ }
+	modifier adminCheck { if (msg.sender == admin) _ }
 	
+	struct DaoNode {
+		string interface;
+		string code;
+	} 
+
+	DaoNode[] daoNodes;
+
 	struct Template {
 		string code;
-		string abi;
+		string interface;
 		string actions;
 		string itemscope;
 		address thesaurus;
@@ -15,32 +22,44 @@ contract core {
 	}
 
 	Template[] templates;
-	mapping (bytes32 => uint) public itemscopeTemplateOf;
-	mapping (bytes32 => bool) public itemscopeTemplateExistOf;
+	mapping (string => uint) public itemscopeTemplateOf;
+	mapping (string => bool) public itemscopeTemplateExistOf;
 
 	function core(string _daoName, string _desc) {
 		daoName = _daoName;
 		desc = _desc;
-		owner = msg.sender;
+		admin = msg.sender;
 	}
 
+	function setDaoNode(string _interface,
+						string _code) adminCheck returns(bool result, uint daoNodeID) {
+		daoNodeID = daoNodes.length++;
+		DaoNode d = daoNodes[daoNodeID];
+		d.interface = _interface;
+		d.code = _code;
+		result = true;
+        return(result, daoNodeID);
+	}
+
+	function getDaoNode() {}
+
 	function setTemplate(string _code,
-						 string _abi,
+						 string _interface,
 						 string _actions,
 						 string _itemscope,
-						 address _thesaurus) ownerCheck returns(bool result, uint templateID) {
+						 address _thesaurus) adminCheck returns(bool result, uint templateID) {
 		templateID = templates.length++;
 		Template t = templates[templateID];
         t.code = _code;
-        t.abi = _abi;
+        t.interface = _interface;
         t.actions = _actions;
         t.itemscope = _itemscope;
         t.thesaurus = _thesaurus;
+        itemscopeTemplateExistOf[t.itemscope] = true;
+        itemscopeTemplateOf[t.itemscope] = templateID;
         result = true;
         return(result, templateID);
 	}
 
-	function getTemplate() {
-		
-	}
+	function getTemplate() {}
 }
