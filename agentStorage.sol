c4e41b4d7941e7e8790eff206e0a6e86268966af
EnsRationis <sergeylonshakov@gmail.com>
2016-01-14 21:30:34 +0400

    Prepare tag 0.3.0
    add agentStorage

---
+++ agentStorage.sol
@@ -0,0 +1,34 @@
+contract agentStorage {
+	address owner;
+
+	modifier ownerCheck { if (msg.sender == owner) _ }
+
+	struct Contract {
+		string itemscope;
+		address contractAddr;
+	}
+	
+	Contract[] contractList;
+	mapping(string => uint) contractOf;
+
+	function agentStorage() {
+		owner = msg.sender;
+	}
+
+	function setContract(string _itemscope, address _contractAddr) ownerCheck returns(bool result, uint contractID) {
+		contractID = contractList.length++;
+		Contract c = contractList[contractID];
+		c.itemscope = _itemscope;
+		c.contractAddr = _contractAddr;
+		contractOf[c.itemscope] = contractID;
+		result = true;
+        return(result, contractID);
+	}
+
+	function getContractID(string _itemscope) ownerCheck returns(uint contractID) {
+		contractID = contractOf[_itemscope];
+		return contractID;
+	}
+
+	function getContractAddr()
+}
