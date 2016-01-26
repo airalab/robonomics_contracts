496dc9f56802f79885e0970e02c771f2cebfe1cf
ensrationis <sergeylonshakov@gmail.com>
2016-01-26 09:51:45 +0400

    fix string -> bytes32 and add upd function

--- agentStorage.sol
+++ agentStorage.sol
@@ -8,8 +8,9 @@ contract agentStorage {
 		address contractAddr;
 	}
 	
-	Contract[] contractList;
-	mapping(string => uint) contractOf;
+	Contract[] public contractList;
+	mapping(bytes32 => uint) public contractOf;
+	mapping(bytes32 => bool) public contractExistOf;
 
 	function agentStorage() {
 		owner = msg.sender;
@@ -20,18 +21,29 @@ contract agentStorage {
 		Contract c = contractList[contractID];
 		c.itemscope = _itemscope;
 		c.contractAddr = _contractAddr;
-		contractOf[c.itemscope] = contractID;
+		contractOf[sha3(c.itemscope)] = contractID;
+		contractExistOf[sha3(c.itemscope)] = true;
 		result = true;
         return(result, contractID);
 	}
 
-	function getContractID(string _itemscope) ownerCheck returns(uint contractID) {
-		contractID = contractOf[_itemscope];
-		return contractID;
+	function getContractID(string _itemscope) ownerCheck returns(bool existing, uint contractID) {
+		if(contractExistOf[sha3(_itemscope)]) {
+			contractID = contractOf[sha3(_itemscope)];
+			existing = true;
+			return (existing, contractID);
+		}
 	}
 
-	function getContractAddr(uint _contractID) returns(address) {
+	function updContractAddr(uint _contractID, string _itemscope, address _contractAddr) ownerCheck returns(bool result) {
 		Contract c = contractList[_contractID];
-		return c.contractAddr;
+		
+		contractExistOf[sha3(c.itemscope)] = false;
+		
+		c.itemscope = _itemscope;
+		c.contractAddr = _contractAddr;
+		contractOf[sha3(c.itemscope)] = _contractID;
+		contractExistOf[sha3(c.itemscope)] = true;
+		return true;
 	}
 }
