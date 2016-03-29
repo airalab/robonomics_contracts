f99d51b986959fcb3d5f54a22d103f006addb5af
Alexander Krupenkin <mail@akru.me>
2016-03-29 13:01:34 +0300

    Added thesaurus library, compilation fixes

--- thesaurus.sol
+++ thesaurus.sol
@@ -1,11 +1,4 @@
 /*
- * Interface for object comparation 
- */
-contract Comparable {
-   function isEqual(Comparable _to) returns (bool);
-}
-
-/*
  * Contract for objects that can be morder
  */
 contract Mortal {
@@ -22,17 +15,19 @@ contract Mortal {
 }
 
 /*
- * Knowledge is a generic declaration of object or process 
+ * Knowledge is a generic declaration of object or process
  */
-contract Knowledge is Comparable, Mortal {
+contract Knowledge is Mortal {
     /* Knowledge can have a type described below */
-    int public OBJECT  = 1;
-    int public PROCESS = 2;
+    int8 constant OBJECT  = 1;
+    int8 constant PROCESS = 2;
+
     /* Knowledge type is a int value */
     int public knowledgeType;
-    
-    /* Constructor gets the type as argument */
-    function Knowledge(int _type) { knowledgeType = _type; }
+
+    function Knowledge(int8 _type) {
+        knowledgeType = _type;
+    }
 
     /* Generic Knowledge comparation procedure */
     function isEqual(Knowledge _to) returns (bool) {
@@ -44,49 +39,79 @@ contract Knowledge is Comparable, Mortal {
          * comparation procedure implemented in the inherit types
          */
         if (knowledgeType == OBJECT) {
-            return KObject(this).isEqual(KObject(_to));
+            return KObject(this).isEqualObject(KObject(_to));
         } else {
-            return KProcess(this).isEqual(KProcess(_to));
+            return KProcess(this).isEqualProcess(KProcess(_to));
         }
     }
 }
 
-contract KObject is Knowledge(Knowledge.OBJECT) {
+/*
+ * The knowledge object represents the real world object
+ * that can be consist of some another objects and have
+ * a count of attributes
+ */
+contract KObject is Knowledge {
+    /* Object constructor */
+    function KObject() Knowledge(OBJECT) {}
 
-    /* Property describe attribute of presented object */
-    struct Property {
-        string name;
-        string value;
+    /* List of object property names */
+    string[] public properties;
+
+    function propertiesLength() returns (uint)
+    { return properties.length; }
+
+    /* Hash name to value mapping */
+    mapping (bytes32 => string)  public propertyValueOf;
+    /* Hash name to value hash mapping */
+    mapping (bytes32 => bytes32) public propertyHashOf;
+
+    /* Insert new property value by name
+     *   If the same name exist value will be replaced
+     */
+    function insertProperty(string _name, string _value) {
+        var nameHash = sha3(_name);
+        // Check for inserting new property
+        if (propertyHashOf[nameHash] == 0)
+            properties[properties.length++] = _name;
+
+        // Store property value and value hash for future comparation
+        propertyValueOf[nameHash] = _value;
+        propertyHashOf[nameHash]  = sha3(_value);
     }
-    /* List of object properties */
-    Property[] public properties;
-    /* Name to value mapping */
-    mapping (string => string) propertyValueOf;
 
     /* Described object can be consist of some another objects */
     KObject[] public components;
 
+    function componentsLength() returns (uint)
+    { return components.length; }
+
+    function appendComponent(KObject _component) {
+        components[components.length++] = _component;
+    }
+
     /*
-     * The equal objects has:
+     * Comparation function over knowledge objects describe equal object,
+     * the equal objects has:
      *  - equal properties
      *  - equal components
      */
-    function isEqual(KObject _to) returns (bool) {
+    function isEqualObject(KObject _to) returns (bool) {
         return isEqualProperties(_to) && isEqualComponents(_to);
     }
 
     function isEqualProperties(KObject _to) returns (bool) {
         // Count of properties in equal objects should be same
-        if (properties.length != _to.properties.length)
+        if (properties.length != _to.propertiesLength())
             return false;
-        
+
         // Compare every property of objects
-        for (var i = 0; i < properties.length; ++i) {
+        for (uint i = 0; i < properties.length; ++i) {
             // Take a name of current property
-            var name = properties[i].name;
+            var nameHash = sha3(properties[i]);
 
             // Compare value of the same properties
-            if (propertyValueOf[name] != _to.propertyValueOf[name])
+            if (propertyHashOf[nameHash] != _to.propertyHashOf(nameHash))
                 return false;
         }
         return true;
@@ -94,15 +119,18 @@ contract KObject is Knowledge(Knowledge.OBJECT) {
 
     function isEqualComponents(KObject _to) returns (bool) {
         // Count of components in equal objects should be same
-        if (components.length != _to.components.length)
+        if (components.length != _to.componentsLength())
             return false;
 
         // Compare every components of objects
-        for (var i = 0; i < components.length; ++i) {
+        for (uint i = 0; i < components.length; ++i) {
             var equalFound = false;
 
-            for (var j = 0; j < _to.components.length; ++j)
-                equalFound |= components[i].isEqual(_to.components[j]);
+            for (uint j = 0; j < components.length; ++j)
+                if (components[i].isEqual(_to.components(j))) {
+                    equalFound = true;
+                    break;
+                }
 
             // Return false if no equal component found
             if (!equalFound)
@@ -112,7 +140,13 @@ contract KObject is Knowledge(Knowledge.OBJECT) {
     }
 }
 
-contract KProcess is Knowledge(Knowledge.PROCESS) {
+/*
+ * The knowledge process describe knowledge manipulation
+ */
+contract KProcess is Knowledge {
+    /* Process constructor */
+    function KProcess() Knowledge(PROCESS) {}
+
     /*
      * Morphism describe knowledge manipulation line
      * e.g. apple production have a morphism with 
@@ -120,48 +154,49 @@ contract KProcess is Knowledge(Knowledge.PROCESS) {
      * this knowledges can be stored in morphism list
      * as [ Ground, AppleTree, Apple ]
      */
-    Knowledge[] morphism;
+    Knowledge[] public morphism;
+
+    function morphismLength() returns (uint)
+    { return morphism.length; }
 
     function append(Knowledge _knowledge) {
         morphism[morphism.length++] = _knowledge;
     }
 
-    function isEqual(KProcess _to) {
+    function isEqualProcess(KProcess _to) returns (bool) {
         // Count of knowledges in equal processes should be same
-        if (morphism.length != _to.morphism.length)
+        if (morphism.length != _to.morphismLength())
             return false;
 
-        for (var i = 0; i < morphism.length; ++i)
+        for (uint i = 0; i < morphism.length; ++i)
             // All knowledge in morphism line should be equal
-            if (!morphism[i].isEqual(_to.morphism[i]))
+            if (!morphism[i].isEqual(_to.morphism(i)))
                 return false;
         return true;
     }
 }
 
-contract KnowledgeIndex {
-    /* Available knowledge names */ 
-    string[] public thesaurus;
-    
-    /* Mapping to knowledge from name */
-    mapping (string => Knowledge) knowledgeOf;
+library Thesaurus {
+    struct Index {
+        /* Available knowledge names */
+        string[] thesaurus;
+
+        /* Mapping to knowledge from name hash */
+        mapping (bytes32 => Knowledge) knowledgeOf;
+    }
 
     /*
      * Insert knowledge by name
      *   knowledge instance with the same name will be replaced
      */
-    function insert(string _name, Knowledge _knowledge) {
-        knowledgeOf[_name] = _knowledge;
+    function insert(Index storage _ix, string _name, Knowledge _knowledge) {
+        var nameHash = sha3(_name);
 
         // Check when thesaurus already contains name
-        if (!thesaurusCheck(_name))
-            thesaurus[thesaurus.length++] = _name;
-    }
+        if (_ix.knowledgeOf[nameHash] == Knowledge(0x0))
+            _ix.thesaurus[_ix.thesaurus.length++] = _name;
 
-    function thesaurusCheck(string _name) {
-        for (var i = 0; i < thesaurus.length; ++i)
-            if (thesaurus[i] == _name)
-                return true;
-        return false;
+        // Insert new knowledge into mapping
+        _ix.knowledgeOf[nameHash] = _knowledge;
     }
 }
