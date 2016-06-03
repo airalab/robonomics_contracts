858e8c9babef453b76b7fe00617e77effe1604d5
EvgenRad81 <zh_tlt@mail.ru>
2016-06-03 19:08:37 +0400

    Update TokenReal.sol

--- sol/token/TokenReal.sol
+++ sol/token/TokenReal.sol
@@ -3,7 +3,7 @@ import './Token.sol';
 contract TokenReal is Token {
     uint8 public decimals;
 
-    function TokenReal(string _name, string _symbol, uint _count, uint _decimals)
+    function TokenReal(string _name, string _symbol, uint _count, uint8 _decimals)
         Token(_name, _symbol, _count)
     { decimals = _decimals; }
 }
