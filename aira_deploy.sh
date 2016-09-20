fdd8db8df04c79d099099495f058fe1adc421286
Alexander Krupenkin <mail@akru.me>
2016-09-20 13:31:16 +0300

    Fix return code of aira_deploy.sh

--- aira_deploy.sh
+++ aira_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Most
-node -h 1>/dev/null 2>/dev/null && node `dirname $0`/scripts/aira_deploy.js "$@"
+node -h 1>/dev/null 2>/dev/null && node `dirname $0`/scripts/aira_deploy.js "$@" && exit 0
 
 ## Ubuntu
-nodejs -h 1>/dev/null 2>/dev/null && nodejs `dirname $0`/scripts/aira_deploy.js "$@"
+nodejs -h 1>/dev/null 2>/dev/null && nodejs `dirname $0`/scripts/aira_deploy.js "$@" && exit 0
