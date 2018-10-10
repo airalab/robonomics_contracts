aa40965d23be16b62f934f77cd2fdc552feb9cf2
Alexander Krupenkin <mail@akru.me>
2018-10-10 11:41:07 +0300

    Drop unused ethpm file

--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
         mainnet: {
             host: '127.0.0.1',
             port: 8545,
-            network_id: '*' // Match any network id
+            network_id: '1' // Only mainnet
         }
 	
     },
