85128c02cd84f8efa4930de2b1b9e58a25bcc3b4
Alexander Krupenkin <mail@akru.me>
2019-04-15 11:56:15 +0300

    Generation 5 deployment

---
+++ gen5-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'generation': '5',
+
+    'foundation': '0x6EFBA8fb2AC5b6730729a972eC224426a287C3Ad',
+
+    'auction': {
+        'ceilingWei': '10000000000000000000000',
+        'priceFactor': '150',
+        'wallet': '0x1faFdA0d9039E4aABFa7B4E293B1196212A704B7'
+    },
+
+    'xrt': {
+        'initialSupply': '10000000000000000',
+        'genesis': {
+            'foundation': '2000000000000000',
+            'auction':    '7000000000000000',
+            'ambix':      '1000000000000000',
+        }
+    }
+}
