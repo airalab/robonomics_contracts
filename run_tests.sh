911a23f07547b51446c03df927c056bf0ffd6175
romanova-natasha <zabelina.nattaly@gmail.com>
2023-09-18 17:43:01 +0300

    check exit codes for tests runs
    check exit codes for tests runs

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,16 @@ echo "Install dependencies"
 npm install --silence
 
 test_directory=./test
+exit_code=0
 
 for entry in "$test_directory"/*.test.js
 do
   echo "Running test: $entry"
   npx hardhat test --network neonlabs $entry
+  if [ $? -ne 0 ]; then
+    exit_code=$?
+  fi
   sleep 5
 done
+
+exit $exit_code
